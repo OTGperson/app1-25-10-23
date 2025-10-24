32359502a38c7fe3b6ece9c99c1acbd8a21c96a6
OTGperson <dlworud0412@gmail.com>
2025-10-24 21:44:31 +0900

    work

--- backend/src/main/java/com/back/domain/home/controller/HomeController.java
+++ backend/src/main/java/com/back/domain/home/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.back.domain.home.controller;
 
+import com.back.global.rq.Rq;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +13,9 @@ import java.net.UnknownHostException;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class HomeController {
+  private final Rq rq;
 
   @GetMapping("/")
   @ResponseBody
